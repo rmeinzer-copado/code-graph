3351e46efdb1c6e98151ee59402ca749c721a841
Ryan Meinzer <87448730+rmeinzer-copado@users.noreply.github.com>
2024-04-03 19:21:05 +0000

    changed repo order

--- app/api/repo/repositories.ts
+++ app/api/repo/repositories.ts
@@ -1,4 +1,5 @@
 export const RESPOSITORIES = [
+    "https://github.com/rmeinzer-copado/broseph",
     "https://github.com/falkordb/falkordb-py",
     "https://github.com/pallets/click",
     "https://github.com/pallets/flask",
